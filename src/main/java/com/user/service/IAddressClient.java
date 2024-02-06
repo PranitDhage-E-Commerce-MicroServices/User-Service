53abf8bbd90eb7eb8faca56acc6cae7deb215e03
Pranit Dhage <pranitdhage92@gmail.com>
2024-02-06 16:47:13 +0530

    Code Updated

--- src/main/java/com/user/service/IAddressClient.java
+++ src/main/java/com/user/service/IAddressClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 //@FeignClient(url = "http://localhost:8082/address", value = "Address-Client")
-@FeignClient(name = "ADDRESS-SERVICE:8082")
+@FeignClient(name = "ADDRESS-SERVICE")
 public interface IAddressClient {
 
     @GetMapping(
